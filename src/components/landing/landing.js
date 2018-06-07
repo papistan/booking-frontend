77b917d73fa5bf5baf48cc92cac95febbc0bc843
papistan <jpapisan@gmail.com>
2018-06-07 11:00:32 -0700

    Finalizes mobile styling

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -8,13 +8,16 @@ const Landing = () => {
     <div>
       <div className="landing-grid">
         <div className="landing-image-container" />
-        <button>BOOK NOW</button>
+        <button id="book-button">BOOK NOW</button>
         <div className="booking-container" />
         <div className="landing-info">
           <p> Electric Bike Rental</p>
           <p>Book by hour, day or week</p>
           <i className="fa fa-bicycle" />
         </div>
+        <button id="calendar-button">
+          <i className="fa fa-calendar" />
+        </button>
       </div>
     </div>
   );
